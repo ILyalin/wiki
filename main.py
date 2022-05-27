12ca18795182b4095e97a067e16ccb61c089654f
ILyalin <Lyalin05@inbox.ru>
2022-05-27 15:48:58 +0300

    init

---
+++ main.py
@@ -0,0 +1,37 @@
+import logging
+import wikipedia
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_TOKEN = os.getenv('BOT_TOKEN')
+
+logging.basicConfig(level=logging.ERROR)
+
+bot = Bot(token=API_TOKEN, parse_mode='HTML')
+dp = Dispatcher(bot)
+
+
+def getwiki(word: str):
+    wikipedia.set_lang("ru")
+    try:
+        p_wiki = wikipedia.page(word)
+        return f'<b>{p_wiki.title}</b>' + p_wiki.summary[len(p_wiki.title) + 1:]
+    except wikipedia.exceptions.PageError:
+        return f'<i>Такого слова нет в словаре Википедии!</i>'
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply(
+        f'<b>Привет {message.from_user.first_name}</b>\n\nВведи слово для поиска в Википедии:')
+
+
+@dp.message_handler()
+async def wikitext(message: types.Message):
+    await message.answer(getwiki(message.text))
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
